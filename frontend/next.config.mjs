6c753f528e5206c8cacfaa143825166091345ece
Makul Saini <136233553+Sainimakul@users.noreply.github.com>
2026-03-17 15:27:37 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,35 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        has: [
-          {
-            type: 'host',
-            value: 'portfolioadmin.makulsaini.online',
-          },
-        ],
-        destination: '/admin',
-      },
-    ];
-  },
-
-  async redirects() {
-    return [
-      {
-        source: '/admin/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'portfolioweb.makulsaini.online',
-          },
-        ],
-        destination: '/',
-        permanent: false,
-      },
-    ];
-  },
-};
+    reactCompiler: true,
+  }
 
 export default nextConfig;
