bfc81636cd5625181f92def6a83389229da8e32e
Makul Saini <136233553+Sainimakul@users.noreply.github.com>
2026-03-18 08:08:44 +0000

    nodemailer port and sercuse set to false

--- backend/helpers/mailer.js
+++ backend/helpers/mailer.js
@@ -1,17 +1,26 @@
 const nodemailer = require("nodemailer");
 
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  family: 4, // force IPv4
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+  rejectUnauthorized: false,
+}
 });
 
 exports.sendMail = async ({ to, subject, html }) => {
+  await transporter.verify();
+  console.log("SMTP ready");
   await transporter.sendMail({
     from: `"Portfolio Admin" <${process.env.EMAIL_USER}>`,
     to,
