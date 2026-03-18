3986a8c40b258c1eec4287584bfe2995e9c0cdb7
Makul Saini <136233553+Sainimakul@users.noreply.github.com>
2026-03-18 07:30:33 +0000

    nodemailer port and sercuse set to false

--- backend/helpers/mailer.js
+++ backend/helpers/mailer.js
@@ -2,6 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail", // or use SMTP config
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
